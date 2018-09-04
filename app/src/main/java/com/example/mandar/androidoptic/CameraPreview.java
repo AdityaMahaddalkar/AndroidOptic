8504928beedfc2ddab09aea1188b684bf4092cd5
Mandar Pande <mandarpande29@gmail.com>
2018-09-04 09:45:41 +0530

    Untested project

---
+++ app/src/main/java/com/example/mandar/androidoptic/CameraPreview.java
@@ -0,0 +1,40 @@
+package com.example.mandar.androidoptic;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.hardware.Camera;
+
+import java.io.IOException;
+
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+    private SurfaceHolder mHolder;
+    private Camera mCamera;
+    public CameraPreview(Context context, Camera camera)
+    {
+        super(context);
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+    public void surfaceCreated(SurfaceHolder holder)
+    {
+        try {
+            mCamera.setPreviewDisplay(holder);
+            mCamera.startPreview();
+        }
+        catch(IOException e)
+        {
+
+        }
+    }
+    public void surfaceDestroyed(SurfaceHolder holder)
+    {
+
+    }
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h)
+    {
+
+    }
+}
--- app/src/main/java/com/example/mandar/androidoptic/MainActivity.java
+++ app/src/main/java/com/example/mandar/androidoptic/MainActivity.java
@@ -1,13 +1,75 @@
 package com.example.mandar.androidoptic;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+//import android.graphics.Camera;
+import android.hardware.camera2.*;
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Camera mCamera;
+    private CameraPreview mPreview;
+    private boolean checkCameraHardware(Context context)
+    {
+        if(context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+
+        }
+    }
+    public static Camera getCameraInstance()
+    {
+        Camera c = null;
+        try
+        {
+            c = Camera.open();
+        }
+        catch(Exception e)
+        {
+
+        }
+        return c;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        boolean f = checkCameraHardware(this);
+        if(!f)
+        {
+            //Handle True
+        }
+        else
+        {
+            //Handle False
+            //Assuming every hardware has been provided with Camera
+        }
+
+        mCamera = getCameraInstance();
+        mPreview=  new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout)findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+    }
+
+    protected void onPause(){
+        super.onPause();
+        releaseCamera();
+    }
+
+    private void releaseCamera() {
+        if (mCamera != null) {
+            mCamera.release();        // release the camera for other applications
+            mCamera = null;
+        }
     }
 }
